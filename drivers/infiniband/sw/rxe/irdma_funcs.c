ea9527e53b84b10dd05b25a6461bacd6a0699f07
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2017-03-18 05:46:11 -0700

    Fix up whitespace in irdma_funcs.c

--- drivers/infiniband/sw/rxe/irdma_funcs.c
+++ drivers/infiniband/sw/rxe/irdma_funcs.c
@@ -25,68 +25,68 @@ int send_packet(
     struct irdma_context* ic,
     unsigned opcode_num,
     struct irdma_mem* payload,
-		struct rxe_pkt_info* cur_pkt,
+    struct rxe_pkt_info* cur_pkt,
     u8 syndrome,
     u32 psn
 ) {
   struct rxe_pkt_info new_pkt;
   struct sk_buff *skb;
   struct rxe_dev *rxe = to_rdev(ic->qp->ibqp.device);
-	u32 crc = 0;
+  u32 crc = 0;
   int err;
   u32* p;
-	int paylen;
-	int pad;
+  int paylen;
+  int pad;
   bool atomicack = rxe_opcode[opcode_num].mask & RXE_ATMACK_MASK;
 
-	if(unlikely(!(irdma_op[rxe_opcode[opcode_num].irdma_op_num].ack))) {
-		pr_err("Tried to send_packet but specifying a non-ack opcode\n");
-		return -EINVAL;
-	}
+  if(unlikely(!(irdma_op[rxe_opcode[opcode_num].irdma_op_num].ack))) {
+      pr_err("Tried to send_packet but specifying a non-ack opcode\n");
+      return -EINVAL;
+  }
 
-	// allocate packet
-	pad = (-payload->length) & 0x3;
-	paylen = rxe_opcode[opcode_num].length + payload->length + pad + RXE_ICRC_SIZE;
-	skb = rxe->ifc_ops->init_packet(rxe, &ic->qp->pri_av, paylen, &new_pkt);
+  // allocate packet
+  pad = (-payload->length) & 0x3;
+  paylen = rxe_opcode[opcode_num].length + payload->length + pad + RXE_ICRC_SIZE;
+  skb = rxe->ifc_ops->init_packet(rxe, &ic->qp->pri_av, paylen, &new_pkt);
   if(!skb) return -ENOMEM;
-	new_pkt.qp = ic->qp;
-	new_pkt.opcode = opcode_num;
-	new_pkt.mask = rxe_opcode[opcode_num].mask;
-	new_pkt.irdma_op_num = rxe_opcode[opcode_num].irdma_op_num;
-	new_pkt.offset = cur_pkt->offset;  // can I change this to rxe_opcode[opcode_num].offset?
-	new_pkt.paylen = paylen;
-	
-	// fill in bth using the request packet headers
-	memcpy(new_pkt.hdr, cur_pkt->hdr, cur_pkt->offset + RXE_BTH_BYTES);
-	  // third argument could be new_pkt.offset + RXE_BTH_BYTES
-	bth_set_opcode(&new_pkt, opcode_num);
-	bth_set_qpn(&new_pkt, ic->qp->attr.dest_qp_num);
-	bth_set_pad(&new_pkt, pad);
-	bth_set_se(&new_pkt, 0);
-	bth_set_psn(&new_pkt, psn);
-	bth_set_ack(&new_pkt, 0);
-	new_pkt.psn = psn;
+  new_pkt.qp = ic->qp;
+  new_pkt.opcode = opcode_num;
+  new_pkt.mask = rxe_opcode[opcode_num].mask;
+  new_pkt.irdma_op_num = rxe_opcode[opcode_num].irdma_op_num;
+  new_pkt.offset = cur_pkt->offset;  // can I change this to rxe_opcode[opcode_num].offset?
+  new_pkt.paylen = paylen;
 
-	if(new_pkt.mask & RXE_AETH_MASK) {
-		aeth_set_syn(&new_pkt, syndrome);
-		aeth_set_msn(&new_pkt, ic->qp->resp.msn);
-	}
-	if(new_pkt.mask & RXE_ATMACK_MASK) {
-		atmack_set_orig(&new_pkt, ic->qp->resp.atomic_orig);
-	}
+  // fill in bth using the request packet headers
+  memcpy(new_pkt.hdr, cur_pkt->hdr, cur_pkt->offset + RXE_BTH_BYTES);
+  // third argument could be new_pkt.offset + RXE_BTH_BYTES
+  bth_set_opcode(&new_pkt, opcode_num);
+  bth_set_qpn(&new_pkt, ic->qp->attr.dest_qp_num);
+  bth_set_pad(&new_pkt, pad);
+  bth_set_se(&new_pkt, 0);
+  bth_set_psn(&new_pkt, psn);
+  bth_set_ack(&new_pkt, 0);
+  new_pkt.psn = psn;
 
-	err = rxe->ifc_ops->prepare(rxe, &new_pkt, skb, &crc);
-	if(err) {
-		kfree_skb(skb);
-		return -ENOMEM;
-	}
+  if(new_pkt.mask & RXE_AETH_MASK) {
+      aeth_set_syn(&new_pkt, syndrome);
+      aeth_set_msn(&new_pkt, ic->qp->resp.msn);
+  }
+  if(new_pkt.mask & RXE_ATMACK_MASK) {
+      atmack_set_orig(&new_pkt, ic->qp->resp.atomic_orig);
+  }
 
-	if(payload) {
-		err = rxe_mem_copy(payload->mr, payload->va, payload_addr(&new_pkt), payload->length, from_mem_obj, &crc);
+  err = rxe->ifc_ops->prepare(rxe, &new_pkt, skb, &crc);
+  if(err) {
+      kfree_skb(skb);
+      return -ENOMEM;
+  }
+
+  if(payload) {
+    err = rxe_mem_copy(payload->mr, payload->va, payload_addr(&new_pkt), payload->length, from_mem_obj, &crc);
     if(err) pr_err("Failed copying memory\n");  // but for some reason, keep going
-	}
-	p = payload_addr(&new_pkt) + payload->length + bth_pad(&new_pkt);
-	*p = ~crc;
+  }
+  p = payload_addr(&new_pkt) + payload->length + bth_pad(&new_pkt);
+  *p = ~crc;
 
   err = send_packet_raw(ic, &new_pkt, skb, rxe, atomicack);
   if(err) pr_err_ratelimited("Failed sending packet with opcode %s\n", rxe_opcode[opcode_num].name);
